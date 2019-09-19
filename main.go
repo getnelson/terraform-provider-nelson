b97afcddd0f2e0b8c2678157426aa4cae3a19469
Timothy Perrett <hello@timperrett.com>
2019-09-19 09:56:24 -0700

    Fix naming

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	// Change to getnelson when opening PR
-	nelson "github.com/drewgonzales360/terraform-provider-nelson/provider"
+	nelson "github.com/getnelson/terraform-provider-nelson/provider"
 
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
